9a8e491928ba7135ba5f7a14cfb3cf326b7734fb
Edward Wilkie <Edward Wilkie>
2016-10-23 18:46:08 +0100

    Simple Command Interface

--- JavaParser/src/suite/SimpleJAnalysisSuite.java
+++ JavaParser/src/suite/SimpleJAnalysisSuite.java
@@ -1,7 +1,10 @@
 package suite;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import com.github.javaparser.JavaParser;
@@ -17,38 +20,38 @@ public class SimpleJAnalysisSuite {
 	private List<Analyzer> analyzers;
 	private CompilationUnit cu;
 
-	public SimpleJAnalysisSuite(CompilationUnit cu) {
-		this.cu = cu;
+	public SimpleJAnalysisSuite() {
 		analyzers = new JsonAnalyzerParser().getAnalyzersFromJson("json/analyzers.json");
 	}
 
-	public void analyzeClassDiagram() {
+	public void analyzeClassDiagram(CompilationUnit cu, Object arg) {
 		print("Class Diagram");
 		for (Analyzer a : analyzers)
-			if (a.equals(SimpleClassDiagramAnalyzer.class))
-				a.analyze(cu, null);
+			if (a.getClass().equals(SimpleClassDiagramAnalyzer.class))
+				a.analyze(cu, arg);
+
 	}
 
-	public void analyzeBadSmells() {
+	public void analyzeBadSmells(CompilationUnit cu, Object arg) {
 		print("Bad Smell");
 		for (Analyzer a : analyzers)
-			if (a.equals(SimpleBadSmellsAnalyzer.class))
-				a.analyze(cu, null);
+			if (a.getClass().equals(SimpleBadSmellsAnalyzer.class))
+				a.analyze(cu, arg);
 
 	}
 
-	public void analyzeMetrics() {
+	public void analyzeMetrics(CompilationUnit cu, Object arg) {
 		print("Software Metrics");
 		for (Analyzer a : analyzers)
-			if (a.equals(SimpleMetricAnalyzer.class))
-				a.analyze(cu, null);
+			if (a.getClass().equals(SimpleMetricAnalyzer.class))
+				a.analyze(cu, arg);
 	}
 
-	public void analyzeAll() {
+	public void analyzeAll(CompilationUnit cu, Object arg) {
 		System.out.println("Running Multiple Analyzers...");
 		print("Class Diagram");
 		for (Analyzer a : analyzers)
-			a.analyze(cu, null);
+			a.analyze(cu, arg);
 
 	}
 
@@ -61,19 +64,57 @@ public class SimpleJAnalysisSuite {
 
 	public static void main(String[] args) throws Exception {
 		// creates an input stream for the file to be parsed
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		SimpleJAnalysisSuite testSuit = new SimpleJAnalysisSuite();
 		File dir = new File("testsuite");
-		for (File f : dir.listFiles()) {
-			FileInputStream in = new FileInputStream(f);
+		System.out.println("Welcome to the SimpleJSuitAnalysisTool.");
+		System.out.println("=======================================\n");
+		System.out.println("Press (i) for instructions.");
+		String input = "";
+		while ((input = br.readLine()) != null) {
+			for (File f : dir.listFiles()) {
+				FileInputStream in = new FileInputStream(f);
 
-			CompilationUnit cu;
-			try {
-				cu = JavaParser.parse(in);
-				SimpleJAnalysisSuite testSuit = new SimpleJAnalysisSuite(cu);
-				testSuit.analyzeAll();
-			} finally {
-				in.close();
+				CompilationUnit cu;
+				try {
+					cu = JavaParser.parse(in);
+
+					switch (input.toLowerCase().charAt(0)) {
+					case 'a':
+						testSuit.analyzeAll(cu, null);
+						break;
+					case 'm':
+						testSuit.analyzeMetrics(cu, null);
+						break;
+					case 'b':
+						testSuit.analyzeBadSmells(cu, null);
+						break;
+					case 'c':
+						testSuit.analyzeClassDiagram(cu, null);
+						break;
+					case 'i':
+						System.out.println("These are the commands for This simple suite:\n");
+						System.out.println("(C)lass diagram analysis");
+						System.out.println("(B)ad Smell analysis");
+						System.out.println("(M)etrics analsis");
+						System.out.println("(A)nalyze All");
+						System.out.println("(I)nstructions");
+						System.out.println("(Q)uit");
+						break;
+					case 'q':
+						System.out.println("Quit");
+						System.exit(0);
+						break;
+					default:
+						System.out.println("Wrong Input");
+						break;
+					}
+
+				} finally {
+					in.close();
+				}
 			}
+			System.out.println("\nEnter Command:");
 		}
 	}
-
 }
